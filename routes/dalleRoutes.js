bc2e07539056ff17c3ff056706c7e40ebfd27231
Pabllot07 <pabllot07@outlook.com>
2023-01-23 17:12:38 -0300

    syntax error fixed

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -26,14 +26,14 @@ router.route('/').post(async(req, res) => {
             size: '1024x1024',
             response_format: 'b64_json'
         })
-        console.log(data)
+
         const image = aiResponse.data.data[0].b64_json;
 
         res.status(200).json({ photo: image })
 
     } catch (error) {
         console.log(error)
-        res.status(500).send(error?.response.data.error.message)
+        res.status(500).send(error?.response?.data.error.message)
     }
 })
 
