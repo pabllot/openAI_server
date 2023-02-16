9a6f90dbfcdd47c206282987e220db8bab31c046
Pabllot07 <pabllot07@outlook.com>
2023-02-16 13:51:58 -0300

    code pattern

---
+++ controllers/post.js
@@ -0,0 +1,40 @@
+import Post from '../mongodb/models/post.js'
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary'
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+export const getPosts = async (req,res) => {
+    try {
+        const posts = await Post.find({})
+
+        res.status(200).json({success: true, data: posts })
+    } catch (error) {
+        res.status(500).json({success: false, message: error })
+        
+    }
+};
+
+export const newPost = async (req,res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newPost = await Post.create({
+            name, 
+            prompt,
+            photo: photoUrl.url
+        })
+
+        res.status(201).json({ success: true, data: newPost });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error})
+    }
+};
