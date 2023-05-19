4cff5286bd86a50275bfd2a2295ec2a9ba235480
Pabllot07 <pabllot07@outlook.com>
2023-05-19 12:10:27 -0300

    new api key

--- controllers/dalle.js
+++ controllers/dalle.js
@@ -1,31 +1,30 @@
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai'
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY    
-})
+  apiKey: "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",
+});
 
 const openai = new OpenAIApi(configuration);
 
-export const createImage = async(req, res) => {
-    try {
-        const { prompt } = req.body;
+export const createImage = async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-        const aiResponse = await openai.createImage({
-            prompt, 
-            n: 1,
-            size: '1024x1024',
-            response_format: 'b64_json'
-        })
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
 
-        const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data.data[0].b64_json;
 
-        res.status(200).json({ photo: image })
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error?.response?.data.error.message)
-    }
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response?.data.error.message);
+  }
 };
