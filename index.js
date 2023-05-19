cd375578953dcf9e1ca544e9467d159b4a50e1ca
Pabllot07 <pabllot07@outlook.com>
2023-05-19 12:01:44 -0300

    new api key

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import connectDB from './mongodb/connect.js'
-import postRoutes from './routes/postRoutes.js'
-import dalleRoutes from './routes/dalleRoutes.js'
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.get('/', async (req,res) => {
-    res.send('Hello World!!!')
-})
+app.get("/", async (req, res) => {
+  res.send("Hello World!!!");
+});
 
-const PORT = 'https://server-ai-pi.vercel.app/' || 8080
+const PORT = "https://server-ai-pi.vercel.app/" || 8080;
 
 const startServer = async () => {
-    try {
-        connectDB(process.env.MONGODB_URL)
-        app.listen(PORT, () => console.log('Server has started on port' + PORT))
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log("Server has started on port " + 8080));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
