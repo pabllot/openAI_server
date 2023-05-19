2e446c158415edc1c38036ad0f8573245966109b
Pabllot07 <pabllot07@outlook.com>
2023-05-19 12:15:33 -0300

    new api key

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = "https://server-ai-pi.vercel.app/" || 8080;
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log("Server has started on port " + 8080));
+    app.listen(PORT, () => console.log("Server has started on port " + PORT));
   } catch (error) {
     console.log(error);
   }
